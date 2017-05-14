847f2225bb12e23dccb7705f159cfea3c01f8817
manny jucaban <manny jucaban>
2017-05-13 23:05:28 -0700

    Added test for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,16 @@
 const request = require('supertest');
 const expect = require('expect');
-
+const { ObjectID } = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 // Seed data
 const todos = [{
+  _id: new ObjectID(),
   text: "First test todo"
 }, {
+  _id: new ObjectID(),
   text: "Second test todo"
 }];
 
@@ -24,42 +26,42 @@ describe('Post todos', () => {
     var text = "Test text";
 
     request(app)
-    .post('/todos')
-    .send({text})
-    .expect(200)
-    .expect((res) => {
-      expect(res.body.text).toBe(text);
-    })
-    .end((err, res) => {
-      if(err){
-        return done(err);
-      }
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
 
-      Todo.find({text}).then((todos) => {
-        expect(todos.length).toBe(1);
-        expect(todos[0].text).toBe(text);
-        done()
-      }).catch((e) => done(e));
-    });
+        Todo.find({text}).then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
   });
 
   it('should not create to do with invalid data', (done) => {
     var text = "";
 
     request(app)
-    .post('/todos')
-    .send(text)
-    .expect(400)
-    .end((err, res) => {
-      if(err){
-        return done(err);
-      }
-      Todo.find().then((todos) => {
-        expect(todos.length).toBe(2);
-        done();
-      }).catch((e) => done(e));
+      .post('/todos')
+      .send(text)
+      .expect(400)
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(2);
+          done();
+        }).catch((e) => done(e));
+      });
     });
-  });
 });
 
 
@@ -72,6 +74,33 @@ describe('Get todos', () => {
       .expect((res) => {
         expect(res.body.todos.length).toBe(2);
       })
-      .end(done());
+      .end(done);
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done)
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/aser2353124143')
+      .expect(404)
+      .end(done);
   });
 });
